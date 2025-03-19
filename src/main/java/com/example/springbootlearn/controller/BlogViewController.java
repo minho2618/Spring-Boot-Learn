adf4185ceae5a6ea819d7c0232cc800f133c3747
minho.lee <minho2618@naver.com>
2025-03-19 16:57:17 +0900

    git commit and push 테스트

---
+++ src/main/java/com/example/springbootlearn/controller/BlogViewController.java
@@ -0,0 +1,25 @@
+package com.example.springbootlearn.controller;
+
+import com.example.springbootlearn.dto.ArticleListViewResponse;
+import com.example.springbootlearn.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequiredArgsConstructor
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream().map(ArticleListViewResponse::new).toList();
+        model.addAttribute("articles", articles);
+
+        return "articlesList";
+    }
+}
